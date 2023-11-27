e8770ea25a530a7cd98e11f7a88905e63afa568f
angelika <dehainaut.angelique@orange.fr>
2023-11-27 22:01:15 +0100

    [test][FEAT][AD] - test with token user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,7 +6,6 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use http\Message;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -30,10 +29,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         maxMessage: "L'email doit comporter au maximum {{ limit }} caractères"
     )]
     #[Assert\Email(message: "L'email n'est pas valide")]
-//    #[Assert\Regex(
-//        '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(fr|com)$',
-//        message: "Votre email doit contenir un @ et doit finir par .fr ou .com"
-//    )]
+    #[Assert\Regex(
+        "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/",
+        message: "Votre email doit contenir un @ et doit finir par minimum 2 caractères et maximum 3"
+    )]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -43,10 +42,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-//    #[Assert\Regex(
-//        '(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
-//        message: "Votre mot de passe doit contenir un caractère majuscule, minuscule et un caractère spécial"
-//    )]
+    #[Assert\Regex(
+        '/^(?=.*[A-Z])(?=.*[a-z])(?=.*[#?!@$%^&*.\\-]).{8,}$/',
+        message: "Votre mot de passe doit contenir un caractère majuscule, minuscule et un caractère spécial, 8caractères"
+    )]
     private ?string $password = null;
 
     #[ORM\Column(nullable: true)]
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -8,23 +8,44 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class UserControllerTest extends webTestCase
 {
     private $client;
+    private $adminToken;
+    private $userToken;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
+        $this->adminToken = $this->getAdminToken();
+        $this->userToken = $this->getUserToken();
     }
 
-   public function testToken():void
+   public function getAdminToken(): mixed
+   {
+        $data = [
+            'email' => 'angedehain@gmailcom',
+            'password' => 'passwordAzerty1!',
+        ];
+        $this->client->request('POST', '/api/login_check', [], [],
+            [
+            'CONTENT_TYPE' => 'application/json',
+            ],
+            json_encode([$data]));
+       $response = json_decode($this->client->getResponse()->getContent(), true);
+        return $response['token'];
+    }
+
+    public function getUserToken(): mixed
     {
         $data = [
             'email' => 'angedehain@gmailcom',
             'password' => 'passwordAzerty1!'
         ];
-        $this->client->request('GET', '/api/login_check', [], [], [], json_encode($data));
-        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+        $this->client->request('POST', '/api/login_check', [], [],
+            [
+            'CONTENT_TYPE' => 'application/json'
+            ],
+            json_encode([$data]));
         $response = json_decode($this->client->getResponse()->getContent(), true);
-        dd($response);
-        $token = $response['token'];
+        return $response['token'];
     }
 
     public function testCreateUser():void
@@ -52,7 +73,12 @@ class UserControllerTest extends webTestCase
 
     public function testGetAll()
     {
-        $this->client->request('GET', '/api/user/');
+        $adminToken = $this->getAdminToken();
+        $this->assertNotNull($adminToken);
+
+        $this->client->request('GET', '/api/user/', [], [], [
+            'HTTP_AUTHORIZATION' => 'Bearer ' . $adminToken,
+        ]);
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
         $response = json_decode($this->client->getResponse()->getContent(), true);
         $this->assertIsArray($response);
